91542b28a62ec5daa67b7e289ae845d700e3626d
SannaAb <sannaabrahamsson@gmail.com>
2023-08-24 07:52:59 +0200

    u

--- NanoPore_pipeline/FilterViralOutForPCRSpilling_SCOPE_old.py
+++ NanoPore_pipeline/FilterViralOutForPCRSpilling_SCOPE_old.py
@@ -2,10 +2,9 @@
 
 # This script takes all final annotated outputs from binned and annotated files and filters breakpoints found in multiple samples, this is an contamination. The script saves the breakpoint with most reads, then outputs filtered files together with a table telling with breakpoints have been removed from the different samples. 
 
+
 # Here we use a bin of + -10 from the samples, the easiest thing to do is to merge the files and then loop throught them sorted by amount of reads. Using the highest evidence as the reference level 
 
-# Difference to vs1
-# Instead of looping through file twice loop through the dictinary keys we create. Will it be faster? 
 
 import sys
 import argparse
@@ -70,6 +69,7 @@ def ParseTheScopeFiles(metadata):
     return(Tot,outMerged, ToRemove)
 
 
+
 def readMergedFromScope(outMerged, sample, ToRemove):
     """
     Here we read the merged files to get a merged file sorted by the sum reads per group. It is a tab delimited file containing the sum of reads across the integration, integration coord. Group and all the integrations within this integration supporting it. 
@@ -84,42 +84,41 @@ def readMergedFromScope(outMerged, sample, ToRemove):
         d={}
         coords=[]
         counter=0
-        with open(i, "r") as Groups: # First open it to save the coords to have something to comapare it with
+        with open(i, "r") as firstround: # First open it to save the coords to have something to comapare it with
             detec=[]
-            for l in Groups:
+            for l1 in firstround:
                 counter+=1
-                l=l.strip()
-                lcoord=l.split("\t")[2]+"-" + l.split("\t")[3]
-                llchrom=l.split("\t")[2].split(":")[0]
-                lrchrom=l.split("\t")[3].split(":")[0]
-                llpos=int(l.split("\t")[2].split(":")[-1])
-                lrpos=int(l.split("\t")[3].split(":")[-1])
-                lsample=l.split("\t")[0]
-                lnreads=int(l.split("\t")[1])
-                lanno1=l.split("\t")[4]
-                lanno2=l.split("\t")[5]
-
-                if not d.keys(): # The dictionary is empty so we either way need to add to the dict
-                    d[lcoord]=[[lnreads,llchrom,llpos,lrchrom,lrpos,lanno1,lanno2, lsample]]
-                    detec.append(l)
-                    
-                else:
-                    for dcoord in d.keys(): # For every line we check the directory if we are within the rage in the dictionary we append it, and we add it to the detec list
-                        dlchrom=dcoord.split(":")[0]
-                        drchrom=dcoord.split("-")[-1].split(":")[0]
-                        dlpos=int(dcoord.split("-")[0].split(":")[-1])
-                        drpos=int(dcoord.split(":")[-1])
-                        if dlchrom == llchrom:
-                            if drchrom == lrchrom:
-                                if abs(llpos - dlpos) <= 10 and abs(lrpos - drpos) <= 10:
-                                    if not l in detec: 
-                                        detec.append(l)
-                                        d[dcoord].append([lnreads,llchrom,llpos,lrchrom,lrpos,lanno1,lanno2, lsample])
-
-                if not l in detec: # if the line is not in the detect list the break is new therefore we add it by itself in the dictory
-                    d[lcoord]=[[lnreads,llchrom,llpos,lrchrom,lrpos,lanno1,lanno2, lsample]]
-                    detec.append(l)
-                    
+                l1=l1.strip()
+                l1coord=l1.split("\t")[2]+"-" + l1.split("\t")[3]
+                l1lchrom=l1.split("\t")[2].split(":")[0]
+                l1rchrom=l1.split("\t")[3].split(":")[0]
+                l1lpos=int(l1.split("\t")[2].split(":")[-1])
+                l1rpos=int(l1.split("\t")[3].split(":")[-1])
+                l1sample=l1.split("\t")[0]
+                l1nreads=int(l1.split("\t")[1])
+                l1anno1=l1.split("\t")[4]
+                l1anno2=l1.split("\t")[5]
+                if not l1 in detec: 
+                    detec.append(l1)
+                    d[l1coord]=[[l1nreads,l1lchrom,l1lpos,l1rchrom,l1rpos,l1anno1,l1anno2, l1sample]]
+                    with open(i, "r") as secondround: # Second loop through the same file to be able to compare the previous hit to all the others, if within rane 
+                        for l2 in secondround:
+                            l2=l2.strip()
+                            l2coord=l2.split("\t")[2]+"-" + l2.split("\t")[3]
+                            l2lchrom=l2.split("\t")[2].split(":")[0]
+                            l2rchrom=l2.split("\t")[3].split(":")[0]
+                            l2lpos=int(l2.split("\t")[2].split(":")[-1])
+                            l2rpos=int(l2.split("\t")[3].split(":")[-1])
+                            l2sample=l2.split("\t")[0]
+                            l2nreads=int(l2.split("\t")[1])
+                            l2anno1=l2.split("\t")[4]
+                            l2anno2=l2.split("\t")[5]
+                            if not l2 in detec: 
+                                if l1lchrom==l2lchrom and l1rchrom == l2rchrom:
+                                    if abs(l1lpos - l2lpos) <= 10 and abs(l1rpos - l2rpos) <= 10:
+                                        detec.append(l2)
+                                        d[l1coord].append([l2nreads,l2lchrom,l2lpos,l2rchrom,l2rpos,l2anno1,l2anno2, l2sample])
+        
         tot=0
         group=i.split("_Merged_Sorted.tmp")[0]
         outfile=group+"_Parsed.tmp"
@@ -149,8 +148,8 @@ def readMergedFromScope(outMerged, sample, ToRemove):
     os.system(command)
     
     return(out_parsedunfilt, ToRemove)
-
-
+        
+        
 
 def ParseAcrossTheSampleGroupsScope(Tot,out_parsedunfilt, sample, ToRemove):
     """
@@ -158,13 +157,6 @@ def ParseAcrossTheSampleGroupsScope(Tot,out_parsedunfilt, sample, ToRemove):
     We can loop through this merged sorted file again twice just as we did above, just keep track on the time.. it might be very slow!
 
     Loop trough the rows, if there is one within  +-10 coords range, if there is different groups within the same range but with the same nreads keep them both! Only discard when one is higher! 
-
-    Important: if there is shared amount of reads in two groups with the same integration keeping them both! 
-    
-    Outputs are: 
-    * DiscardedHitsFromContamination_Bin10.txt : Those were removed from the PCR filtering
-    * MergedParsedFilteredFromContamination_Bin10.txt: Filtered with the PCR filtering in a merged file, In this file you can see the amount of reads in each sample as well and which samples that integration is detected in
-    * SCOPE_PCRfilt.txt Contains the filtered files in the same format as the input
 
     Outputs are the common FiltForContaminations_groups_Bin.txt nreads\tlcorrds\trcoord\tanno1\tanno2
     x The ParsedFiles output with removed contaminants! (New)
@@ -175,80 +167,44 @@ def ParseAcrossTheSampleGroupsScope(Tot,out_parsedunfilt, sample, ToRemove):
     logging.info('%s\tComparing the integrations between the groups',time.ctime().split(" ")[-2])
     OutParsed=sample+"_MergedParsedFilteredFromContamination_Bin10.txt"
     OutDiscarded=sample+"_DiscardedHitsFromContamination_Bin10.txt"
-
     
     with open(out_parsedunfilt, "r") as firstround, open(OutParsed, "w") as oMerged, open(OutDiscarded, "w") as oDiscard: # First open it to save the coords to have something to comapare it with
         detec=[]
-        d={}
         #discard=[]
-        for l in firstround:
-            l=l.strip()
-            lcoord=l.split("\t")[1]
-            llchrom=l.split("\t")[1].split(":")[0]
-            lrchrom=l.split("\t")[1].split("-")[-1].split(":")[0]
-            llpos=int(l.split("\t")[1].split("-")[0].split(":")[-1])
-            lrpos=int(l.split("\t")[1].split("-")[-1].split(":")[-1])           
-            lnreads=str(l.split("\t")[0])
-            lgroup=str(l.split("\t")[2])
-            lall=l.split("\t")[-1]
-            dkey=lcoord+"_"+lgroup+"_"+lnreads
-            if not d.keys(): # The dictionary is empty so we either way need to add to the dict
-                d[dkey]=[[lgroup+","+lall]]
-                detec.append(l)
-                
-            else:
-                for key in d.keys(): # For every line we check the directory if we are within the rage in the dictionary we append it, and we add it to the detec list
-                    
-                    dlchrom=key.split(":")[0]
-                    drchrom=key.split("-")[-1].split(":")[0]
-                    dlpos=int(key.split("-")[0].split(":")[-1])
-                    drpos=int(key.split(":")[-1].split("_")[0])
-                    dnreads=int(key.split("_")[-1])
-                    
-                    if dlchrom == llchrom:
-                        if drchrom == lrchrom:
-                            if abs(llpos - dlpos) <= 10 and abs(lrpos - drpos) <= 10:
-                                if not l in detec:
-                                    if int(lnreads) < int(dnreads): # If the amount of reads is the same as previous we dont want to append it as we are not filtering them away. It will be a seperate hit in the dictionary instead
-                                        d[key].append([lgroup+","+lall])                            
-                                        detec.append(l)
+        for l1 in firstround:
+            l1=l1.strip()
+            l1coord=l1.split("\t")[1]
+            l1lchrom=l1.split("\t")[1].split(":")[0]
+            l1rchrom=l1.split("\t")[1].split("-")[-1].split(":")[0]
+            l1lpos=int(l1.split("\t")[1].split("-")[0].split(":")[-1])
+            l1rpos=int(l1.split("\t")[1].split("-")[-1].split(":")[-1])           
+            l1nreads=int(l1.split("\t")[0])
+            if not l1 in detec: # As l1 comes first, if it is not in detect always print to the parsed file! 
+                detec.append(l1)
+                print(l1, file=oMerged)
+                with open(out_parsedunfilt, "r") as secondround: # Second loop through the same file to be able to compare the previous hit to all the others, if within rane
+                    for l2 in secondround:
+                       l2=l2.strip()
+                       l2coord=l2.split("\t")[1]
+                       l2lchrom=l2.split("\t")[1].split(":")[0]
+                       l2rchrom=l2.split("\t")[1].split("-")[-1].split(":")[0]
+                       l2lpos=int(l2.split("\t")[1].split("-")[0].split(":")[-1])
+                       l2rpos=int(l2.split("\t")[1].split("-")[-1].split(":")[-1])
+                       l2nreads=int(l2.split("\t")[0])
+                       if not l2 in detec:
+                            if l1lchrom==l2lchrom and l1rchrom == l2rchrom:
+                                if abs(l1lpos - l2lpos) <= 10 and abs(l1rpos - l2rpos) <= 10:
+                                    detec.append(l2)
+                                    if l2nreads < l1nreads: # If l2 is within the same range as l1 but smaller we discard it!
+                                        filtstring=l2.split("\t")[3].replace("|","\n").replace(",","\t")
+                                        print(filtstring, file=oDiscard)
+                                    else:
+                                        print(l2, file=oMerged) # if l2 is not smaller it can just mean it is the same size, then we still keep it!
 
-                                        
-            if not l in detec: # if the line is not in the detect list the break is new therefore we add it by itself in the dictory
-                detec.append(l)
-                d[dkey]=[[lgroup+","+lall]]
-
-        print("nreads_tot\tcoords\tgroup\tsamples", file=oMerged)                
-        for key, values in d.items(): # Looping through the key dict to save the outouts:
-            ntotalreads=str(key.split("_")[-1])
-            ngrouptosave=str(key.split("_")[2])
-            lcoord=key.split("-")[0]
-            rcoord=key.split("-")[-1].split("_")[0]
-            coord=lcoord+"-"+rcoord
-            if len(values) == 1: #  only one hit so no need to filter, just print it to the files
-                for i in values:
-                    details=",".join(i[0].split(",")[1:])
-                    print(ntotalreads+"\t"+coord+"\t"+ngrouptosave+"\t"+details, file=oMerged)
-                    
-                    
-            else: # We have multiple groups to the same barcode, here we need to perform filtering!
-                # We know which group we are saving from the key, just extract it from the values
-                for i in values:
-                    group=str(i[0].split(",")[0])
-                    if group == ngrouptosave:
-                        details=",".join(i[0].split(",")[1:])
-                        print(ntotalreads+"\t"+coord+"\t"+ngrouptosave+"\t"+details, file=oMerged)
-                    else: # If there are multiple groups to the same bin but the hit group is not the same as the one saved in key we put it in discard!
-                        # Obs, we should only discard when one is higher! Otherwise we keep them both!                        
-                        for t in ",".join(i[0].split(",")[1:]).split("|"): # We want one sample on each row for quick filtering
-                            discardstring=t.replace(",","\t")
-                            print(discardstring, file=oDiscard)
-                            
 
     # Gathering statistics to make sure the input integrations is the same as the sum of reported (filtered and Discarded)
     TotFilt=0
     with open(OutParsed, "r") as inf:
-        next(inf)
         for l in inf:
             l=l.strip()
             c=int(l.split("\t")[0])
@@ -260,20 +216,29 @@ def ParseAcrossTheSampleGroupsScope(Tot,out_parsedunfilt, sample, ToRemove):
             l=l.strip()
             c=int(l.split("\t")[0])
             TotDiscarded+=c
-
+            
     logging.info('%s\tnFiltered integrations (Reads):%s \tnDiscarded Integrations (Reads): %s', time.ctime().split(" ")[-2],TotFilt, TotDiscarded)
     logging.info('%s\tnTotal output integrations (Reads): %s\t nTotal input integrations (Reads): %s', time.ctime().split(" ")[-2],TotFilt+TotDiscarded, Tot)
 
+    # Parsing the report, one filtered per barcode, extracted from the OutParsed!
+    # As we are parsing with echo (and append) we need to remove outputfiles if they are there
+    """
+    currentdir=os.getcwd()
+    print(currentdir)
+    res = glob.glob(currentdir+"/*_FiltForContaminationBetweenSameGroups_Bin10.txt")
+    if res:
+        logging.info('%s\tWarning, there is already outputs from SCOPE in Folder, removing these now!', time.ctime().split(" ")[-2])
+        for i in res:
+            os.remove(i)
+    """
 
-    # Finally we loop through the Merged parsed file to split the integrations to their files
     Detected=[]
     with open(OutParsed, "r") as inf:
-        next(inf)
         for l in inf:
             l=l.strip()
             for r in l.split("\t")[3].split("|"):
                 row=r.replace(",","\t")
-                sample=row.split("\t")[-1].split(".txt")[0]+"_SCOPE_PCRfilt.txt"
+                sample=row.split("\t")[-1].split("_Binned_10nt_Annotated.txt")[0]+"_FiltForContaminationBetweenSameGroups_Bin10.txt"
                 outrow=row.split("\t")[0]+"\t"+ row.split("\t")[1] + ":" + row.split("\t")[2] + "\t" +  row.split("\t")[3] + ":" +  row.split("\t")[4] +"\t" +  row.split("\t")[5] + "\t" + row.split("\t")[6]
                 if not sample in Detected: # This is the first time we reach it! Create the file and the header
                     command="echo \"nSupportedReads\tleftmostCoords\trightmostCoords\tAnno\tAnno2\" > " + sample
@@ -285,6 +250,15 @@ def ParseAcrossTheSampleGroupsScope(Tot,out_parsedunfilt, sample, ToRemove):
                     command="echo \""+outrow+ "\" >> "+sample
                     os.system(command)
 
+
+    logging.info('%s\tCleaning', time.ctime())
+
+    # Remove the tmps
+    for r in ToRemove:
+        os.remove(r)
+                    
+
+                    
 def main(metadata, sample):
     # Create the logger
     process = psutil.Process(os.getpid())
